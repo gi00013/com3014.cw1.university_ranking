c00abb05c72ffc969f03538976741d826e804ad7
Giorgos Ioannidis <gi00013@surrey.ac.uk>
2014-11-04 17:27:58 +0000

    a

--- src/java/com3014/service/UniversityService.java
+++ src/java/com3014/service/UniversityService.java
@@ -3,15 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com3014.controller;
+package com3014.service;
 
 /**
  *
- * @author 
+ * @author giorgosioannidis
  */
-public class UniversityController {
-    
-    public String name;
-    
+public class UniversityService {
     
 }
